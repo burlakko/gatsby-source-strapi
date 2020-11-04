fc026fa98d9486e12ec7bde8a59b8b1320383510
Danny Matkovsky <booodyk@gmail.com>
2020-11-04 18:16:08 +0200

    + fixed build

---
+++ authentication.js
@@ -0,0 +1,91 @@
+'use strict'
+
+var _regenerator = require('babel-runtime/regenerator')
+
+var _regenerator2 = _interopRequireDefault(_regenerator)
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator')
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2)
+
+var _fp = require('lodash/fp')
+
+var _axios = require('axios')
+
+var _axios2 = _interopRequireDefault(_axios)
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+
+module.exports = (function() {
+  var _ref = (0, _asyncToGenerator3.default)(
+    /*#__PURE__*/ _regenerator2.default.mark(function _callee(_ref2) {
+      var loginData = _ref2.loginData,
+        reporter = _ref2.reporter,
+        apiURL = _ref2.apiURL
+      var validIndentifier, validPassword, authenticationActivity, loginResponse
+      return _regenerator2.default.wrap(
+        function _callee$(_context) {
+          while (1) {
+            switch ((_context.prev = _context.next)) {
+              case 0:
+                validIndentifier = (0, _fp.has)('identifier', loginData) && loginData.identifier.length !== 0
+                validPassword = (0, _fp.has)('password', loginData) && loginData.password.length !== 0
+
+                if (!(validIndentifier && validPassword)) {
+                  _context.next = 17
+                  break
+                }
+
+                authenticationActivity = reporter.activityTimer('Authenticate Strapi User')
+
+                authenticationActivity.start()
+
+                // Make API request.
+                _context.prev = 5
+                _context.next = 8
+                return _axios2.default.post(apiURL + '/auth/local', loginData)
+
+              case 8:
+                loginResponse = _context.sent
+
+                authenticationActivity.end()
+
+                if (!(0, _fp.has)('data', loginResponse)) {
+                  _context.next = 12
+                  break
+                }
+
+                return _context.abrupt('return', loginResponse.data.jwt)
+
+              case 12:
+                _context.next = 17
+                break
+
+              case 14:
+                _context.prev = 14
+                _context.t0 = _context['catch'](5)
+
+                reporter.panic('Strapi authentication error: ' + _context.t0)
+
+              case 17:
+                return _context.abrupt('return', null)
+
+              case 18:
+              case 'end':
+                return _context.stop()
+            }
+          }
+        },
+        _callee,
+        undefined,
+        [[5, 14]]
+      )
+    })
+  )
+
+  return function(_x) {
+    return _ref.apply(this, arguments)
+  }
+})()
---
+++ fetch.js
@@ -0,0 +1,108 @@
+'use strict'
+
+var _regenerator = require('babel-runtime/regenerator')
+
+var _regenerator2 = _interopRequireDefault(_regenerator)
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator')
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2)
+
+var _axios = require('axios')
+
+var _axios2 = _interopRequireDefault(_axios)
+
+var _lodash = require('lodash')
+
+var _pluralize = require('pluralize')
+
+var _pluralize2 = _interopRequireDefault(_pluralize)
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+
+module.exports = (function() {
+  var _ref = (0, _asyncToGenerator3.default)(
+    /*#__PURE__*/ _regenerator2.default.mark(function _callee(_ref2) {
+      var apiURL = _ref2.apiURL,
+        contentType = _ref2.contentType,
+        singleType = _ref2.singleType,
+        jwtToken = _ref2.jwtToken,
+        queryLimit = _ref2.queryLimit,
+        reporter = _ref2.reporter
+
+      var apiBase, apiEndpoint, _ref3, data
+
+      return _regenerator2.default.wrap(
+        function _callee$(_context) {
+          while (1) {
+            switch ((_context.prev = _context.next)) {
+              case 0:
+                // Define API endpoint.
+                apiBase = singleType ? apiURL + '/' + singleType : apiURL + '/' + (0, _pluralize2.default)(contentType)
+                apiEndpoint = apiBase + '?_limit=' + queryLimit
+
+                reporter.info('Starting to fetch data from Strapi - ' + apiEndpoint)
+
+                _context.prev = 3
+                _context.next = 6
+                return (0, _axios2.default)(apiEndpoint, addAuthorizationHeader({}, jwtToken))
+
+              case 6:
+                _ref3 = _context.sent
+                data = _ref3.data
+                return _context.abrupt('return', (0, _lodash.castArray)(data).map(clean))
+
+              case 11:
+                _context.prev = 11
+                _context.t0 = _context['catch'](3)
+
+                reporter.panic('Failed to fetch data from Strapi', _context.t0)
+
+              case 14:
+              case 'end':
+                return _context.stop()
+            }
+          }
+        },
+        _callee,
+        undefined,
+        [[3, 11]]
+      )
+    })
+  )
+
+  return function(_x) {
+    return _ref.apply(this, arguments)
+  }
+})()
+
+/**
+ * Remove fields starting with `_` symbol.
+ *
+ * @param {object} item - Entry needing clean
+ * @returns {object} output - Object cleaned
+ */
+var clean = function clean(item) {
+  ;(0, _lodash.forEach)(item, function(value, key) {
+    if ((0, _lodash.startsWith)(key, '__')) {
+      delete item[key]
+    } else if ((0, _lodash.startsWith)(key, '_')) {
+      delete item[key]
+      item[key.slice(1)] = value
+    } else if ((0, _lodash.isObject)(value)) {
+      item[key] = clean(value)
+    }
+  })
+
+  return item
+}
+
+var addAuthorizationHeader = function addAuthorizationHeader(options, token) {
+  if (token) {
+    ;(0, _lodash.set)(options, 'headers.Authorization', 'Bearer ' + token)
+  }
+
+  return options
+}
